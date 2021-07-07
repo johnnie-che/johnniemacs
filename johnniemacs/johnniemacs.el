e62ceba12fcec58f1e1d32992e731d6d3c5934be
Eugene Cherkasov <eugene.v.cherkasov@gmail.com>
2021-07-07 12:06:22 +0700

    Add backup files config

--- johnniemacs/johnniemacs.el
+++ johnniemacs/johnniemacs.el
@@ -1,5 +1,15 @@
 (provide 'johnniemacs)
 
+;; Setup bakup options
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.emacs.d/backups/"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)
+
 ;; Add MELPA repo and refresh
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
